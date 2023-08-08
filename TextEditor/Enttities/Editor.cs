696cc07168fe252de8e90eb6fe783911219417a3
Leandro Guimaraes <leandroguim@hotmail.com>
2023-08-08 12:09:25 -0300

    Criada a função salvar

---
+++ TextEditor/Enttities/Editor.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextEditor.Enttities
+{
+    class Editor
+    {
+        public void ShowMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("O que você deseja fazer?");
+            Console.WriteLine("1 - Abrir arquivo");
+            Console.WriteLine("2 - Criar novo arquivo");
+            Console.WriteLine("0 - Sair");
+            short option = short.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 0: System.Environment.Exit(0); break;
+                case 1: Abrir(); break;
+                case 2: Editar(); break;
+                default: ShowMenu(); break;
+            }
+        }
+
+        public void Abrir()
+        {
+
+        }
+
+        public void Editar()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite seu texto abaixo (ESC para sair):");
+            Console.WriteLine("---------------------------------------");
+            string text = "";
+
+            do
+            {
+                text += Console.ReadLine();
+                text += Environment.NewLine;
+            }
+            while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+            Salvar(text);
+
+        }
+
+        public void Salvar(string text)
+        {
+            Console.Clear();
+            Console.WriteLine("Qual o caminho para salvar o arquivo?");
+            var path = Console.ReadLine();
+
+            using (var file = new StreamWriter(path)) 
+            {
+                file.WriteLine(text);
+            }
+            Console.WriteLine($"Arquivo {path} salvo com sucesso!");
+            ShowMenu();
+        }
+    }
+}
--- TextEditor/Program.cs
+++ TextEditor/Program.cs
@@ -1,10 +1,6 @@
 ﻿
+using TextEditor.Enttities; 
 
-static void Menu()
-{
-    Console.Clear();
-    Console.WriteLine("O que você deseja fazer?");
-    Console.WriteLine("1 - Abrir arquivo");
-    Console.WriteLine("2 - Criar novo arquivo");
-    Console.WriteLine("0 - Sair");
-}
+Editor editor = new Editor();
+
+editor.ShowMenu();
