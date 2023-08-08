2b4e607ef47265f9819af84a09b6399128a626e8
Leandro Guimaraes <leandroguim@hotmail.com>
2023-08-08 10:28:28 -0300

    Add project files.

---
+++ TextEditor/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+static void Menu()
+{
+    Console.Clear();
+    Console.WriteLine("O que você deseja fazer?");
+    Console.WriteLine("1 - Abrir arquivo");
+    Console.WriteLine("2 - Criar novo arquivo");
+    Console.WriteLine("0 - Sair");
+}
